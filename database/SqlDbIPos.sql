290fa1daf1541def30e89d17d30bfb476e4a8bff
NguyenTung <vantung7992@gmail.com>
2018-08-24 22:05:02 +0700

    Update db

--- database/SqlDbIPos.sql
+++ database/SqlDbIPos.sql
@@ -5,50 +5,50 @@ Use IPOS;
 --Create table
 ---Product
 create table Products(
-	Code varchar(50),
+	ID bigint,
 	[Name] nvarchar(200),
 	Product_Category_ID int,
 	Base_Unit_ID int,
-	constraint Products_P primary key(Code)
+	constraint Products_P primary key(ID)
 );
 ---Product_Group: Nhóm sản phẩm (1 sản phẩm được tạo thành từ nhiều loại sp khác)
 create table Product_Categories(
-	ID int,
+	ID bigint,
 	[Name] nvarchar(200),
 	Parent_ID int
 	constraint Product_Group_P primary key(ID)	
 );
 ---Product_Image
 create table Product_Image(
-	 ID int,
-	 Product_Code varchar(50),
+	 ID bigint,
+	 Product_ID varchar(50),
 	 Contents text,
 	 Number int,
 	 constraint Product_Image_P primary key(ID)
 );
 ---Product_Unit
 Create table Product_Unit(
-	ID int,
 	Product_Code varchar(50),
+	Product_ID bigint,
 	[Name] nvarchar(200),
 	Sell_Price decimal(18,0),
 	Original_Price decimal(18,0),
-	Base_Unit_ID int,
-	Number int,
-	constraint Product_Unit_P primary key(ID)
+	Base_Product_Code varchar(50),
+	QuantityPerUnit int,
+	constraint Product_Unit_P primary key(Product_Code)
 );
 ---Transaction_Detail
 create table Transaction_Detail(
 	Transaction_Code varchar(50),
-	Product_Unit_ID int,
-	Number int,
+	Product_Code bigint,
+	Quantity int,
 	Price decimal(18,0),
 	Total decimal(18,0),
-	constraint Transaction_Detail_P primary key(Transaction_Code,Product_Unit_ID)
+	constraint Transaction_Detail_P primary key(Transaction_Code,Product_Code)
 );
 ---Transaction_Type_Group
 create table Transaction_Type_Group(
-	ID int,
+	ID bigint,
 	Type varchar(50),
 	[Name] nvarchar(200),
 	constraint Transaction_Type_Group_P primary key(ID)
@@ -61,8 +61,8 @@ create table [Transaction](
 	Type_Group_ID int,
 	Created_Time date,
 	Created_User varchar(50),
-	Partner_ID int,
-	Shop_ID int,
+	Partner_ID bigint,
+	Shop_ID bigint,
 	Discount decimal(18,0),
 	Note nvarchar(500),
 	constraint Transaction_P primary key(Code)
@@ -70,7 +70,7 @@ create table [Transaction](
 ---Transaction_Payment
 ----Payment type: M - Cash, C - Card, D - Dept
 create table Transaction_Payment(
-	ID int,
+	ID bigint,
 	Transaction_Code varchar(50),
 	[Type] varchar(50),
 	Amount decimal(18)
@@ -79,7 +79,7 @@ create table Transaction_Payment(
 ---Shop
 ----Partner type: C - Customer, S - Supplier
 create table Shop(
-	ID int,
+	ID bigint,
 	[Name] nvarchar(200),
 	Phone varchar(50),
 	Email varchar(50),
@@ -89,7 +89,7 @@ create table Shop(
 );
 ---Partner
 create table [Partner](
-	ID int,
+	ID bigint,
 	[Name] nvarchar(200),
 	Phone varchar(50),
 	Email varchar(50),
@@ -108,28 +108,28 @@ create table Users(
 	Email varchar(50),
 	[Address] nvarchar(200),
 	Birthday int,
-	Role_ID int,
+	Role_ID bigint,
 	Login_Time_Retrict bit,
 	[Status] varchar(1),
 	constraint Users_P primary key(Account)
 );
 ---Role
 create table [Role](
-	ID int,
+	ID bigint,
 	[Name] nvarchar(200),
 	constraint Role_P primary key(ID)
 );
 ---User_Permission
 create table User_Permission(
 	User_Account varchar(50),
-	Shop_ID int,
+	Shop_ID bigint,
 	Permission_Code varchar(50),
 	Has_Permission bit,
 	constraint User_Permission_P primary key(User_Account,Shop_ID,Permission_Code)
 );
 ---Role_Permission
 create table Role_Permission(
-	Role_ID int,
+	Role_ID bigint,
 	Permission_Code varchar(50),
 	Has_Permission bit,
 	constraint Role_Permission_P primary key(Role_ID,Permission_Code)
