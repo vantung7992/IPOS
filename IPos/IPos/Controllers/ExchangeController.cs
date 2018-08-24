912153951ef8b7ab2e5f352827f9400187867281
TUNGNV <vantung7992@gmail.com>
2018-08-24 17:31:58 +0700

    checkin de ve nha lam hehe

--- IPos/IPos/Controllers/ExchangeController.cs
+++ IPos/IPos/Controllers/ExchangeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using IPos.Models;
 
 namespace IPos.Controllers
 {
@@ -14,6 +15,54 @@ namespace IPos.Controllers
             return View();
         }
 
+        private const string BillSession = "BillSession";
+        #region Bill
+        //Add item
+        public ActionResult AddItem(string productCode, int quantity)
+        {
+            var product = GetProductbyCode(productCode);
+            if (product == null)
+            {
+                return null; //TODO
+            }
+
+
+            var bill = Session[BillSession];
+            if (bill != null)
+            {
+
+            }
+            else
+            {
+                var item = new BillItem()
+                {
+                    ProductCode = productCode,
+                    Quantity = quantity,
+                    Price = 0
+                };
+                
+            }
+        }
+
+        //Get product by code
+        private Products GetProductbyCode(string ProductCode)
+        {
+            using (var dbContext = new IPOSEntities())
+            {
+                return dbContext.Products.Where(x => x.Code == ProductCode).FirstOrDefault();
+            }
+        }
+
+        #endregion
+
 
     }
+    [Serializable]
+    public class BillItem
+    {
+        public string ProductCode { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+
 }
